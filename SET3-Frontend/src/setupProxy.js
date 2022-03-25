46df1e7f25a9999a6351a69281e790232e57e63c
Muhamed Omerović <momerovic4@etf.unsa.ba>
2022-03-25 20:19:36 +0100

    Initial Project Commit

---
+++ SET3-Frontend/src/setupProxy.js
@@ -0,0 +1,14 @@
+﻿const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const context = [
+    "/weatherforecast",
+];
+
+module.exports = function (app) {
+    const appProxy = createProxyMiddleware(context, {
+        target: 'https://localhost:7194',
+        secure: false
+    });
+
+    app.use(appProxy);
+};
