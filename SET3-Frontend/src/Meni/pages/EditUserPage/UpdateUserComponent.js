1f6f38a59835ed428aec62797b28a3c81419e03a
nsabotic1 <72942574+nsabotic1@users.noreply.github.com>
2022-03-28 20:41:04 +0200

    created PUT for users

--- SET3-Frontend/src/Meni/pages/EditUserPage/UpdateUserComponent.js
+++ SET3-Frontend/src/Meni/pages/EditUserPage/UpdateUserComponent.js
@@ -10,7 +10,12 @@ import { useHistory, useParams } from 'react-router-dom'
 
 const UpdateUserComponent = (props) => {
     const [user, setUser] = useState(null);
+    const [anUser, setanUser] = useState(null);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setemail] = useState("");
     const [edit, setEdit] = useState(false);
+    const [fetch, setFetch] = useState(false);
     const { id } = useParams();
 
     const getData = async () => {
@@ -20,6 +25,9 @@ const UpdateUserComponent = (props) => {
         const data = await response.json();
         console.log(data);
         setUser(data);
+        setFirstName(data.firstName);
+        setLastName(data.lastName);
+        setemail(data.email);
     }
 
     useEffect(getData, [])
@@ -28,9 +36,37 @@ const UpdateUserComponent = (props) => {
         setEdit(true);
     }
 
-    const handleSaveButtonClikc = () => {
-        setEdit(false);
+    const handleSaveButtonClikc = async () => {
+        const newUser = {
+            id: user.id,
+            email: user.email,
+            firstName: firstName,
+            lastName: user.lastName,
+            password: user.password,
+            question: user.question,
+            questionId: user.questionId,
+            answer: user.answer,
+            deleted: user.deleted
+        }
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newUser)
+        };
+
+        const response = await fetch('https://localhost:7194/usermodels/' + user.id, requestOptions);
+        const data = await response.json();
+
+        setanUser(data);
+
+
     }
+
+    useEffect(async () => {
+
+        setUser(anUser);
+    }, [anUser])
+
 
     const handleCancelButtonClikc = () => {
         setEdit(false);
@@ -40,8 +76,17 @@ const UpdateUserComponent = (props) => {
         disabled: true,
         hidden: true
     }
+    const handleSetFirstName = (event) => {
+        setFirstName(event.target.value);
 
-
+    }
+    const handleSetLastName = (event) => {
+        setLastName(event.target.value);
+    }
+    const handleSetemail = (event) => {
+        setemail(event.target.value);
+    }
+   
     return (
         <>
             {!user && <div>Loading...</div>}
@@ -50,9 +95,9 @@ const UpdateUserComponent = (props) => {
 
                     <div className="container" >
                         <div className="updateUser-box">
-                            <input type="text" value={user.firstName} disabled={state.disabled}></input>
-                            <input type="text" value={user.lastName} disabled={state.disabled}></input>
-                            <input type="text" value={user.email} disabled={state.disabled}></input>
+                            <input type="text" value={firstName} disabled={!edit} onChange={handleSetFirstName}></input>
+                            <input type="text" value={lastName} disabled={!edit} onChange={ handleSetLastName}></input>
+                            <input type="text" value={email} disabled={!edit} onChange={ handleSetemail}></input>
                             <Stack direction="column" spacing={2} height="30%" width="40%" margin="2%" >
                                 {!edit && <Button className="editButton" variant="outlined" onClick={handleEditButtonClikc} >Edit</Button>}
                                 {edit && <div>
