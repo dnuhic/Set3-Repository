3e6ae168d7fafa3c3feb2411547490a6d4f81f29
Amila Begović <abegovic6@etf.unsa.ba>
2022-03-28 19:40:42 +0200

    the page is opening

--- SET3-Frontend/src/Meni/pages/EditUserPage/UpdateUserComponent.js
+++ SET3-Frontend/src/Meni/pages/EditUserPage/UpdateUserComponent.js
@@ -2,107 +2,86 @@
 import './UpdateUserComponent.css';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom'
 
 
-class UpdateUserComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        //this.id = this.props.match.params.id;
-        this.user = null;
+
+
+const UpdateUserComponent = (props) => {
+    const [user, setUser] = useState(null);
+    const [edit, setEdit] = useState(false);
+    const { id } = useParams();
+
+    const getData = async () => {
+        
+        const response = await fetch('https://localhost:7194/usermodels/' + id);
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        setUser(data);
     }
 
-    compoentDidMount() {
-        const id = this.props.match.params.id;
-        console.log("OVDJE JE " + id);
-        console.log(window.location.hash.split('/')[2], 'this is my id')
-        fetch('https://localhost:7194/usermodels/' + id)
-            .then(res => res.json)
-            .then(json => {
-                this.setUser(json);
-                console.log("povucen korisnik");
-            });
-    }
-    
+    useEffect(getData, [])
 
-    state = {
+    const handleEditButtonClikc = () => {
+        setEdit(true);
+    }
+
+    const handleSaveButtonClikc = () => {
+        setEdit(false);
+    }
+
+    const handleCancelButtonClikc = () => {
+        setEdit(false);
+    }
+
+    const state = {
         disabled: true,
         hidden: true
-
     }
 
 
+    return (
+        <>
+            {!user && <div>Loading...</div>}
+            {user && <div className="App">
+                <div className="main-box">
 
-    handleChange = (e) => {
-        if (e.target) {
-            this.setState({
-                disabled: !this.state.disabled,
-                hidden: !this.state.hidden
-            });
+                    <div className="container" >
+                        <div className="updateUser-box">
+                            <input type="text" value={user.firstName} disabled={state.disabled}></input>
+                            <input type="text" value={user.lastName} disabled={state.disabled}></input>
+                            <input type="text" value={user.email} disabled={state.disabled}></input>
+                            <Stack direction="column" spacing={2} height="30%" width="40%" margin="2%" >
+                                {!edit && <Button className="editButton" variant="outlined" onClick={handleEditButtonClikc} >Edit</Button>}
+                                {edit && <div>
+                                    <Button className="submitButton" variant="outlined" onClick={handleSaveButtonClikc}>Submit</Button>
+                                    <Button className="cancelButton" variant="outlined" onClick={handleCancelButtonClikc} >Cancel</Button>
 
-        }
+                                </div>}
 
+                                
 
-
-    }
-
-    showButton = (e) => {
-        if (e.target) {
-            this.setState({
-                disabled: !this.state.disabled,
-                hidden: !this.state.hidden
-            });
-
-
-
-
-
-        }
-
-    }
-    render() {
-        return (
-            <>
-                {!this.user && <div>Loading...</div>}
-                {this.user && <div className="App">
-                    <div className="main-box">
-
-                        <div className="container" >
-                            <div className="updateUser-box">
-                                <input type="text" value={this.user.firstName} disabled={this.state.disabled}></input>
-                                <input type="text" value={this.user.lastName} disabled={this.state.disabled}></input>
-                                <input type="text" value={this.user.email} disabled={this.state.disabled}></input>
-                                <Stack direction="column" spacing={2} height="30%" width="40%" margin="2%" >
-                                    <Button className="editButton" variant="outlined" onClick={this.showButton} >Edit</Button>
-
-                                    <div hidden={this.state.hidden} >
-                                        <Button className="submitButton" variant="outlined" onClick={this.handleChange}>Submit</Button>
-                                        <Button className="cancelButton" variant="outlined" onClick={this.handleChange} >Cancel</Button>
-
-                                    </div>
-
-                                </Stack>
-                            </div>
-
-                            <div className="edit-role">
-                                <p>edit role </p>
-
-                            </div>
+                            </Stack>
                         </div>
 
-                        <div className="updatePassword-box">
-                            <p>Ovdje će ići promjena passworda.</p>
+                        <div className="edit-role">
+                            <p>edit role </p>
+
                         </div>
                     </div>
 
-                </div>}
+                    <div className="updatePassword-box">
+                        <p>Ovdje će ići promjena passworda.</p>
+                    </div>
+                </div>
 
-                
-            </>
+            </div>}
 
-            
-        );
-    }
+
+        </>
+    );
 }
 
 export default UpdateUserComponent;
