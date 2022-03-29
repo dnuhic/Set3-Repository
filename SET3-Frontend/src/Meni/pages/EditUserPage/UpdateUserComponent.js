00bdaaa378a73d0a7e2d30973bef2d4260e56acc
Amila Begović <abegovic6@etf.unsa.ba>
2022-03-29 09:52:14 +0200

    the view is opening

--- SET3-Frontend/src/Meni/pages/EditUserPage/UpdateUserComponent.js
+++ SET3-Frontend/src/Meni/pages/EditUserPage/UpdateUserComponent.js
@@ -3,31 +3,30 @@ import './UpdateUserComponent.css';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import React, { useState, useEffect } from 'react';
-import { useHistory, useParams } from 'react-router-dom'
-
-
-
+import {useParams } from 'react-router-dom'
 
 const UpdateUserComponent = (props) => {
     const [user, setUser] = useState(null);
-    const [anUser, setanUser] = useState(null);
+    const [updatedUser, setUpdatetUser] = useState(null);
+
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
-    const [email, setemail] = useState("");
+    const [email, setEmail] = useState("");
+
     const [edit, setEdit] = useState(false);
-    const [fetch, setFetch] = useState(false);
     const { id } = useParams();
 
     const getData = async () => {
-        
+        console.log(id);
         const response = await fetch('https://localhost:7194/usermodels/' + id);
         console.log(response);
         const data = await response.json();
         console.log(data);
+
         setUser(data);
         setFirstName(data.firstName);
         setLastName(data.lastName);
-        setemail(data.email);
+        setEmail(data.email);
     }
 
     useEffect(getData, [])
@@ -36,56 +35,56 @@ const UpdateUserComponent = (props) => {
         setEdit(true);
     }
 
-    const handleSaveButtonClikc = async () => {
+    const handleSaveButtonClikc = () => {
+        console.log("USER: " + user);
+        console.log(user);
         const newUser = {
-            id: user.id,
-            email: user.email,
-            firstName: firstName,
-            lastName: user.lastName,
-            password: user.password,
-            question: user.question,
-            questionId: user.questionId,
-            answer: user.answer,
-            deleted: user.deleted
+            "Id": user.Id,
+            "Email": document.getElementById("email").value,
+            "FirstName": document.getElementById("firstName").value,
+            "LastName": document.getElementById("lastName").value,
+            "Password": user.Password,
+            "Question": user.Question,
+            "QuestionId": user.QuestionId,
+            "Answer": user.Answer,
+            "Deleted": user.Deleted
         }
-        const requestOptions = {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(newUser)
-        };
 
-        const response = await fetch('https://localhost:7194/usermodels/' + user.id, requestOptions);
-        const data = await response.json();
-
-        setanUser(data);
-
-
+        setUpdatetUser(newUser);
+        setEdit(false);
     }
 
     useEffect(async () => {
+        console.log(updatedUser);
 
-        setUser(anUser);
-    }, [anUser])
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(updatedUser)
+        };
+
+        const response = await fetch('https://localhost:7194/usermodels/1', requestOptions);
+        const data = await response.json();
+        console.log(data);
+    }, [updatedUser])
 
 
     const handleCancelButtonClikc = () => {
         setEdit(false);
     }
 
-    const state = {
-        disabled: true,
-        hidden: true
-    }
-    const handleSetFirstName = (event) => {
+    const handleFirstNameChange = (event) => {
         setFirstName(event.target.value);
-
     }
-    const handleSetLastName = (event) => {
+
+    const handleLastNameChange = (event) => {
         setLastName(event.target.value);
     }
-    const handleSetemail = (event) => {
-        setemail(event.target.value);
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
     }
+
    
     return (
         <>
@@ -95,9 +94,9 @@ const UpdateUserComponent = (props) => {
 
                     <div className="container" >
                         <div className="updateUser-box">
-                            <input type="text" value={firstName} disabled={!edit} onChange={handleSetFirstName}></input>
-                            <input type="text" value={lastName} disabled={!edit} onChange={ handleSetLastName}></input>
-                            <input type="text" value={email} disabled={!edit} onChange={ handleSetemail}></input>
+                            <input id="firstName" type="text" value={firstName} disabled={!edit} onChange={handleFirstNameChange}></input>
+                            <input id="lastName" type="text" value={lastName} disabled={!edit} onChange={handleLastNameChange}></input>
+                            <input id="email" type="text" value={email} disabled={!edit} onChange={handleEmailChange}></input>
                             <Stack direction="column" spacing={2} height="30%" width="40%" margin="2%" >
                                 {!edit && <Button className="editButton" variant="outlined" onClick={handleEditButtonClikc} >Edit</Button>}
                                 {edit && <div>
