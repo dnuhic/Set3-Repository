36af39259dda24fcf07d056db6539c266aaa641d
aahmic3 <aahmic3@etf.unsa.ba>
2022-03-31 08:14:12 +0200

    Revert "Editovan stil form za pregled rola."
    This reverts commit 1a5339081ece574530c451dc3d9005e3efe355a5.

--- SET3-Frontend/src/AccessRights/AccessRights.js
+++ SET3-Frontend/src/AccessRights/AccessRights.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import './AccessRights/AccessRights.css';
+import './AcessRights.css';
 
+function Naslov() {
+  return (
+    <div className="Naslov">
+      <h1>Promjena prava pristupa za sve korisnike</h1>
+    </div>
+
+  );
+
+}
 
 function App() {
-    return (
-        <div className="App">
-            <div className="Naslov">
-                <h1>Promjena prava pristupa za sve korisnike</h1>
-            </div>
-            <table>
-                <tr>
-                    <th>Tip</th>
-                    <th>Read</th>
-                    <th>Write</th>
-                    <th>Delete</th>
-                </tr>
-            </table>
-        </div>
-    );
+  return (
+    <div className="App">
+      <table>
+        <tr>
+          <th>Tip</th>
+          <th>Read</th>
+          <th>Write</th>
+          <th>Delete</th>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
 let btn = document.createElement("button");
@@ -28,5 +33,5 @@ btn.id = "spremi";
 document.body.appendChild(btn);
 
 
-
+export default Naslov; 
 export default App;
--- SET3-Frontend/src/App.js
+++ SET3-Frontend/src/App.js
@@ -1,32 +1,59 @@
-import React from 'react';
-import './AccessRights/AccessRights.css';
+import React, { Component } from 'react';
 
+export default class App extends Component {
+    static displayName = App.name;
 
-function App() {
-    return (
-        <div className="App">
-            <div className="Naslov">
-                <h1>Promjena prava pristupa za sve korisnike</h1>
-            </div>
-            <table>
-                <tr>
-                    <th>Tip</th>
-                    <th>Read</th>
-                    <th>Write</th>
-                    <th>Delete</th>
-                </tr>
+    constructor(props) {
+        super(props);
+        this.state = { forecasts: [], loading: true };
+    }
+
+    componentDidMount() {
+        this.populateWeatherData();
+    }
+
+    static renderForecastsTable(forecasts) {
+        return (
+            <table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Temp. (C)</th>
+                        <th>Temp. (F)</th>
+                        <th>Summary</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {forecasts.map(forecast =>
+                        <tr key={forecast.date}>
+                            <td>{forecast.date}</td>
+                            <td>{forecast.temperatureC}</td>
+                            <td>{forecast.temperatureF}</td>
+                            <td>{forecast.summary}</td>
+                        </tr>
+                    )}
+                </tbody>
             </table>
-        </div>
-    );
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
+            : App.renderForecastsTable(this.state.forecasts);
+
+        return (
+            <div>
+                <h1 id="tabelLabel" >Weather forecast</h1>
+                <p>This component demonstrates fetching data from the server.</p>
+                {contents}
+            </div>
+        );
+    }
+
+    async populateWeatherData() {
+        const response = await fetch('weatherforecast');
+        const data = await response.json();
+        this.setState({ forecasts: data, loading: false });
+    }
 }
-
-let btn = document.createElement("button");
-btn.innerHTML = "Save";
-btn.type = "submit";
-btn.name = "formBtn";
-btn.id = "spremi";
-document.body.appendChild(btn);
-
-
-
-export default App;
