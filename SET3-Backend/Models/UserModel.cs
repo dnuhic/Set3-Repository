b566941fbb2c656ced2d7f4db23da8d40b51f02a
Amila Begović <abegovic6@etf.unsa.ba>
2022-04-02 00:11:05 +0200

    resolved merge

--- SET3-Backend/Models/UserModel.cs
+++ SET3-Backend/Models/UserModel.cs
@@ -5,18 +5,30 @@ namespace SET3_Backend.Models
 {
     public class UserModel
     {
-		[Key]
-		public int Id { get; set; }
-		[EmailAddress]
-		public string Email { get; set; }
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
-		public string Password { get; set; }
-		public SecurityQuestionModel Question { get; set; }
-		public int QuestionId { get; set; }
-		public string Answer { get; set; }
-		public bool Deleted { get; set; }
-		public int RoleModelId { get; set; }
-		public RoleModel RoleModel { get; set; }
-	}
+        [Key]
+        public int Id { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public int QuestionId { get; set; }
+        public string Answer { get; set; }
+        public bool Deleted { get; set; }
+        public string RoleName { get; set; }
+
+        public UserModel(string email, string firstName, string lastName, string password,
+            string roleName, int questionId, string answer, bool deleted)
+        {
+            Email = email;
+            FirstName = firstName;
+            LastName = lastName;
+            Password = password;
+            RoleName = roleName;
+            QuestionId = questionId;
+            Answer = answer;
+            Deleted = deleted;
+        }
+    }
 }
+        
