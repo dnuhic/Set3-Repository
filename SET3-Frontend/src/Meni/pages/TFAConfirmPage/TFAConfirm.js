d8289ff1f0051bde66535d287eded87361f3b14e
Muhamed Omerović <momerovic4@etf.unsa.ba>
2022-04-03 18:08:32 +0200

    merge

---
+++ SET3-Frontend/src/Meni/pages/TFAConfirmPage/TFAConfirm.js
@@ -0,0 +1,146 @@
+import { emphasize } from '@mui/material';
+import React, {
+    Component, useState, useEffect, useCallback
+} from 'react';
+import '../styleForm.css';
+import { useNavigate, useParams } from 'react-router-dom';
+
+
+
+const TFAConfirm = (props) => {
+
+    const { id } = useParams();
+
+    const [UserCode, setUserCode] = useState("");
+    const navigate = useNavigate();
+    const [Email, setEmail] = useState("");
+
+    function getCookie(key) {
+        var b = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
+        return b ? b.pop() : "";
+    }
+
+    const validCode = () => {
+        let x = validate();
+        console.log(x);
+
+        if (x) {
+
+            alert("Verification successful. TFA activated!");
+
+            const user = { "Email": props.email,"Password": props.password };
+            console.log("Uspjesno pozvano");
+            console.log(JSON.stringify(user));
+
+            fetch(`${process.env.REACT_APP_BACKEND_URL}Authentication`, {
+                method: 'POST',
+                headers: { "Content-Type": "application/json", "Access-Control-Allow-Credentials": true },
+                credentials: 'include',
+                body: JSON.stringify(user)
+            }).then(response => {
+
+                
+                    navigate('/');
+                    window.location.reload(false);
+                    window.location.reload(false);
+                
+            });
+
+
+        } else alert("Wrong code!");
+
+    }
+
+    useEffect(async () => {
+        /*
+        const getEmail = async () => {
+            const emailFromServer = await fetchEmail()
+            setEmail(emailFromServer);
+        }
+        getEmail();
+        */
+        let email = {
+            "ToEmail": props.email
+        };
+        console.log("Email je: ")
+        console.log(email);
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(email)
+        };
+
+        var res = await fetch(`${process.env.REACT_APP_BACKEND_URL}api/mail/sendcode`, requestOptions);
+        console.log(res);
+        var data = await res.json();
+
+        alert("Check your e-mail then verify the code.");
+        console.log(data);
+        setUserCode(data.result);
+
+    }, [])
+    /*
+    const fetchCode = async () => {
+        const res = await fetch('https://localhost:7194/usermodels/' + id, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json", "Access-Control-Allow-Credentials": true,
+                "Authorization": "bearer " + getCookie("jwt")
+            },
+            credentials: 'include'
+        })
+        const data = await res.json();
+        setUserCode(data.TFA);
+        return data;
+    }*/
+    /*
+    const fetchEmail = async () => {
+        const res = await fetch('https://localhost:7194/usermodels/' + id, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json", "Access-Control-Allow-Credentials": true,
+                "Authorization": "bearer " + getCookie("jwt")
+            },
+            credentials: 'include'
+        })
+        const data = await res.json();
+        setEmail(data.email);
+        return data;
+    }*/
+
+
+    const validate = () => {
+        var usercode = document.getElementById("codeinput").value;
+        /*
+        const getCode = async () => {
+            const codeFromServer = await fetchCode()
+            setUserCode(codeFromServer);
+        }
+        getCode();*/
+
+        console.log("Korisnik unio: " + usercode);
+        console.log("Korisnikov tfa: ");
+        console.log(UserCode);
+
+        return usercode === UserCode;
+    }
+
+    return (
+        <div className="form-container">
+            <div name="myForm" className="form-wrap" >
+                <h1>Input 6-digit code: </h1>
+                <div className="form-box">
+                    <input id="codeinput" type="text" placeholder="Input code" required />
+                </div>
+                <div className="form-submit">
+                    <button onClick={validCode}>Confirm</button>
+                </div>
+
+            </div>
+        </div>
+    );
+
+};
+
+export default TFAConfirm;
---
+++ SET3-Frontend/src/Meni/pages/TFAPage/TFA.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
+import '../styleForm.css';
+import TFAConfirm from "../TFAConfirmPage/TFAConfirm";
+
+const TFA = () => {
+    const [user, setUser] = useState(null);
+    const [clicked, setClicked] = useState(false)
+
+    const handleSumbit = () => {
+        setClicked(true);
+
+    }
+
+    const { id } = useParams();
+
+    function getCookie(key) {
+        var b = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
+        return b ? b.pop() : "";
+    }
+
+    const getData = async () => {
+        console.log(id);
+        const requestOptions = {
+            method: 'GET',
+            headers: { "Authorization": "bearer " + getCookie("jwt"), "Access-Control-Allow-Credentials": true },
+            credentials: 'include'
+        };
+        const responseUser = await fetch(`${process.env.REACT_APP_BACKEND_URL}usermodels/${id}`, requestOptions);
+        const dataUser = await responseUser.json();
+
+        setUser(dataUser);
+    };
+
+    useEffect(getData, []);
+   
+    return (
+        <>
+            {user && !clicked && <div className="form-container">
+            <div name="myForm" className="form-wrap" >
+                <h1>Enable Two Factor Authentication</h1>
+                <div className="form-submit">
+                    <button onClick={handleSumbit}>Enable</button>
+                </div>
+            </div>
+            </div>}
+            {user && clicked && <TFAConfirm email={user.email} password={user.password} />}
+            {!user && <h1>Loading...</h1>}
+        </>
+        
+        
+    );
+}
+
+export default TFA;
