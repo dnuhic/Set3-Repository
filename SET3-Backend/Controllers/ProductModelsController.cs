227a53cb1f421a888b2932591e7950e0d5cb8aed
mmarincic1 <mmarincic1@etf.unsa.ba>
2022-04-04 19:50:40 +0200

    created a method for updating a product

--- SET3-Backend/Controllers/ProductModelsController.cs
+++ SET3-Backend/Controllers/ProductModelsController.cs
@@ -1,11 +1,15 @@
 ﻿#nullable disable
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using SET3_Backend.Database;
 using SET3_Backend.Models;
 
@@ -16,10 +20,12 @@ namespace SET3_Backend.Controllers
     public class ProductModelsController : ControllerBase
     {
         private readonly Context _context;
+        private readonly IConfiguration _configuration;
 
-        public ProductModelsController(Context context)
+        public ProductModelsController(Context context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         // GET: api/ProductModels
@@ -41,6 +47,52 @@ namespace SET3_Backend.Controllers
             }
 
             return productModel;
+        }
+
+        // ova metoda se koristi za update/edit proizvoda kao i za brisanje 
+        // na formi za brisanje se salje taj proizvod samo sa promijenjenim atributom deleted na true
+        // i ovdje se to update
+        // provjeriti autorizaciju je li samo admin skladista ovo moze??
+        // metodu sam testirao bez autorizacije i tokena i radi, kada se poveze sa frontendom bice sve ok
+        [HttpPost("{id}"), Authorize(Roles = "StockAdmin")] 
+        public async Task<IActionResult> PostUpdateProductModel(int id, ProductModel productModel)
+        {
+
+            var token = Request.Headers["Authorization"];
+            token = token.ToString().Substring(token.ToString().IndexOf(" ") + 1);
+
+
+            if (ValidateToken(token) != null)
+            {
+                if (id != productModel.Id)
+                {
+                    return BadRequest();
+                }
+
+                
+                _context.Update(productModel);
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (Exception e)
+                {
+
+                    if (!ProductModelExists(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+            else return NoContent();
+
+
+            return NoContent();
         }
 
         // PUT: api/ProductModels/5
@@ -104,6 +156,30 @@ namespace SET3_Backend.Controllers
         private bool ProductModelExists(int id)
         {
             return _context.ProductModels.Any(e => e.Id == id);
+        }
+
+        protected JwtSecurityToken ValidateToken(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            TokenValidationParameters validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value)),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.Zero
+            };
+            try
+            {
+                handler.ValidateToken(token, validationParameters, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                return jwtToken;
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
