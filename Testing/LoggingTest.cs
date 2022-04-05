1b5be1b3a8ebcf8b4e996cddb7331c8e7dabe7fa
DeninaSiljegovic <73441899+DeninaSiljegovic@users.noreply.github.com>
2022-04-06 01:21:09 +0200

    dodani testovi

--- Testing/LoggingTest.cs
+++ Testing/LoggingTest.cs
@@ -71,6 +71,95 @@ namespace Testing
         }
 
 
+        [Test]
+        public async Task PutUserModelTestAsync1()
+        {
+            UserModel user = new UserModel
+            {
+                Id = 3,
+                Email = "zumreta@etf.unsa.ba",
+                FirstName = "Zumy",
+                LastName = "Zum zum",
+                Password = "1234",
+                QuestionId = 3,
+                Answer = "bela",
+                Deleted = false
+            };
+
+            _context.Setup(x => x.UpdateUserModel(user));
+
+            _logger.Verify(x => x.LogInformation("Put UserModel ended"), Times.Once);
+            await _userModelController.PutUserModel(3, user);
+        }
+
+
+        [Test]
+        public async Task PutUserModelTestAsync2()
+        {
+            UserModel user = new UserModel
+            {
+                Id = 12,
+                Email = "itugy@etf.unsa.ba",
+                FirstName = "Iuf",
+                LastName = "Idf",
+                Password = "13234",
+                QuestionId = 12,
+                Answer = "be2la",
+                Deleted = false
+            };
+
+            _context.Setup(x => x.AddUserModel(user));
+
+            _logger.Verify(x => x.LogInformation("Succesfully added new UserModel"), Times.Once);
+            await _userModelController.PostUserModel(user);
+        }
+
+
+        [Test]
+        public async Task DeleteUserModelAsync()
+        {
+            UserModel user = new UserModel
+            {
+                Id = 12,
+                Email = "itugy@etf.unsa.ba",
+                FirstName = "Iuf",
+                LastName = "Idf",
+                Password = "13234",
+                QuestionId = 12,
+                Answer = "be2la",
+                Deleted = false
+            };
+
+            _context.Setup(x => x.DeleteUserModel(user));
+
+            _logger.Verify(x => x.LogInformation("Succesfully deleted UserModel"), Times.Once);
+            await _userModelController.DeleteUserModel(1);
+        }
+
+
+    /*    [Test]
+        public async Task UserModelExistsAsync()
+        {
+            UserModel user = new UserModel
+            {
+                Id = 12,
+                Email = "itugy@etf.unsa.ba",
+                FirstName = "Iuf",
+                LastName = "Idf",
+                Password = "13234",
+                QuestionId = 12,
+                Answer = "be2la",
+                Deleted = false
+            };
+
+            _context.Setup(x => x.UserModelExists(12));
+
+            _logger.Verify(x => x.LogInformation("Succesfully deleted UserModel"), Times.Once);
+            await _userModelController.UserModelExists(12);
+        }
+    */
+
+
 
 
 
