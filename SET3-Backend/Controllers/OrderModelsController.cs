980d61a3e71c52f2bdd2eaa66eab2f5178ee499c
Senija <skaleta1@etf.unsa.ba>
2022-04-11 00:00:53 +0200

    creating new order

--- SET3-Backend/Controllers/OrderModelsController.cs
+++ SET3-Backend/Controllers/OrderModelsController.cs
@@ -1,11 +1,15 @@
 ﻿#nullable disable
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using SET3_Backend.Database;
 using SET3_Backend.Models;
 
@@ -16,10 +20,12 @@ namespace SET3_Backend.Controllers
     public class OrderModelsController : ControllerBase
     {
         private readonly Context _context;
+        private readonly IConfiguration _configuration;
 
-        public OrderModelsController(Context context)
+        public OrderModelsController(Context context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         // GET: api/OrderModels
@@ -85,6 +91,74 @@ namespace SET3_Backend.Controllers
             return CreatedAtAction("GetOrderModel", new { id = orderModel.Id }, orderModel);
         }
 
+        public class ForAddingNewOrder
+        {
+            public int ShopId { get; set; }
+            public List<int> ProductIds { get; set; }
+            public List<int> Quantities { get; set; }
+        }
+
+        //POST: api/order/{shopId}
+        [HttpPost("order/{shopId}")] //, Authorize(Roles = "StockAdmin,Admin")
+        public async Task<ActionResult<List<OrderModel>>> createOrder()
+        {
+            string proba;
+            using (var reader = new StreamReader(Request.Body))
+            {
+                proba = await reader.ReadToEndAsync();
+
+                // Do something
+            }
+
+            ForAddingNewOrder unos = JsonSerializer.Deserialize<ForAddingNewOrder>(proba);
+
+            var products = unos.ProductIds;
+            var quantities = unos.Quantities;
+            var shopId = unos.ShopId;
+
+            OrderModel badRequest = new OrderModel(-1, DateTime.Now, -1, -1); // da se naznaci da je nevalidno?
+            if (products.Count != quantities.Count)
+            {
+                return BadRequest(badRequest);
+
+                //ovo mora biti ispunjeno jer za Order treba nam za svaki proizvod njegova kolicina bez obzira je li to 0 ili !=0
+            }
+
+            List<OrderModel> orderModels = new List<OrderModel>();
+
+            for(int i = 0; i < quantities.Count; i++)
+            {
+                //validacija: je li kolicina u skladistu manja od kolicine koja se trazi
+                if(_context.ProductModels.Find(products[i]).Quantity < quantities[i])
+                {
+                    return BadRequest(badRequest); 
+                }
+                var product = _context.ProductModels.Find(products[i]);
+                product.Quantity = product.Quantity - quantities[i];
+                _context.ProductModels.Update(product); // prepravka kolicine u skladistu 
+
+                // product shop intertable pravljenje ovdje pada
+                var productInShop = _context.ProductShopIntertables.Where(x => x.ShopId == shopId && x.ProductId == products[i]).First();
+                if(productInShop == null)
+                {
+                    _context.ProductShopIntertables.Add(new ProductShopIntertable(shopId, products[i], quantities[i]));
+                }
+                else
+                {
+                    productInShop.Quantity = productInShop.Quantity + quantities[i];
+                    _context.ProductShopIntertables.Update(productInShop);
+                }
+
+                var newOrder = new OrderModel(shopId, DateTime.Now, quantities[i], products[i]);
+
+                _context.OrderModels.Add(newOrder);
+                orderModels.Add(newOrder);
+               
+            }
+            _context.SaveChanges();
+            return orderModels;
+        }
+
         // DELETE: api/OrderModels/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteOrderModel(int id)
@@ -101,9 +175,35 @@ namespace SET3_Backend.Controllers
             return NoContent();
         }
 
+        
+
         private bool OrderModelExists(int id)
         {
             return _context.OrderModels.Any(e => e.Id == id);
+        }
+
+        protected JwtSecurityToken ValidateToken(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            TokenValidationParameters validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value)),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.Zero
+            };
+            try
+            {
+                handler.ValidateToken(token, validationParameters, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                return jwtToken;
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
