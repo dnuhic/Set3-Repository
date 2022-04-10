ba9901a207acb1aeb0afcc55db7ed4af594b39aa
Dzenan Nuhic <dnuhic1@etf.unsa.ba>
2022-04-10 17:57:56 +0200

    Ispravljena klasa ProductShopIntertable

--- SET3-Backend/Models/ProductShopIntertable.cs
+++ SET3-Backend/Models/ProductShopIntertable.cs
@@ -6,8 +6,8 @@ namespace SET3_Backend.Models
     {
         [Key]
         public int Id { get; set; }
-        public int StockId { get; set; }
-        public int SroductId { get; set; }
+        public int ShopId { get; set; }
+        public int ProductId { get; set; }
         public int Quantity { get; set; }
     }
 }
