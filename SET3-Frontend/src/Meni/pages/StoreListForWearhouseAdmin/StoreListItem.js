f12bb15d2724181df8e7fe4e86b98fad6ed04beb
shadzijusu1 <shadzijusu1@etf.unsa.ba>
2022-04-11 21:51:09 +0200

    delete onClick

--- SET3-Frontend/src/Meni/pages/StoreListForWearhouseAdmin/StoreListItem.js
+++ SET3-Frontend/src/Meni/pages/StoreListForWearhouseAdmin/StoreListItem.js
@@ -12,15 +12,15 @@ import { useState, useEffect } from 'react';
 function StoreListItem(props) {
 
     let navigate = useNavigate();
-
+/*
     const handleStoreClick = () => {
             navigate(`/cashRegister/${props.store.id}`);
     }
-
+    */
     return (
         <MuiListItem
             key={props.store.id}>
-            <ListItemText primary={`${props.store.name}`} onClick={handleStoreClick} />
+            <ListItemText primary={`${props.store.name}`} />
         </MuiListItem>
     )
 }
