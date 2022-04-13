679fa88f9a862448816f38ef87fdafe12a6e90ec
Dzenan Nuhic <dnuhic1@etf.unsa.ba>
2022-04-13 03:56:17 +0200

    added more style. connected with backend

--- SET3-Frontend/src/Meni/pages/OrderPage/Orders.js
+++ SET3-Frontend/src/Meni/pages/OrderPage/Orders.js
@@ -1,5 +1,5 @@
 import React, { Component, useEffect, useState } from 'react';
-import { useTable, useSortBy } from 'react-table'
+import { useTable, useSortBy, usePagination } from 'react-table'
 import ResponseCheckModule from "../ErrorPage/ResponseCheckModule"
 import { useNavigate } from "react-router-dom"
 import './OrdersStyle.css'
@@ -10,28 +10,99 @@ function Table({ columns, data }) {
         getTableProps,
         getTableBodyProps,
         headerGroups,
-        rows,
         prepareRow,
+        page,
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        state: { pageIndex, pageSize },
     } = useTable(
         {
             columns,
             data,
+            initialState: { pageIndex: 0, pageSize: 1 },
         },
-        useSortBy
+        useSortBy,
+        usePagination
     )
+
+    function handleFilterButtonClick() {
+        var filterDiv = document.getElementById("hiddenFilterDiv")
+        if (filterDiv.style.display == 'flex')
+            filterDiv.style.display = 'none'
+        else
+            filterDiv.style.display = 'flex'
+    }
 
     return (
         <>
+            <div class="mainFilterDiv">
+                <div class="filterDiv">
+                    <div>
+                        Show&nbsp;
+                        <select
+                            value={pageSize}
+                            onChange={e => {
+                                setPageSize(Number(e.target.value))
+                            }}
+                        >
+                            {[1, 2, 3, 4].map(pageSize => (
+                                <option key={pageSize} value={pageSize}>{pageSize}
+                                </option>
+                            ))}
+                        </select>
+                        &nbsp;entires
+                    </div>
+                    <div>
+                        <button onClick={handleFilterButtonClick}>
+                            <img src={require("./filter.png")} alt="filter icon" width="25" height="25"></img>
+                        </button>
+                    </div>
+                </div>
+                <div id="hiddenFilterDiv">
+                    <div id="insideHiddenFilterDiv">
+                        <div>
+                            <label for="date">Filter by date&nbsp;</label>
+                            <input type="date" name="date" id="date"></input>
+                        </div>
+                        <div>
+                            <label for="name">Filter by name&nbsp;</label>
+                            <input type="text" name="name" id="name"></input>
+                        </div>
+                        <div>
+                            <label for="category">Category&nbsp;</label>
+                            <select id="category">
+                                <option>Fruits and Vegetables</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div id="insideHiddenFilterDiv2">
+                        <div>
+                            <label for="shop">Filter by shop&nbsp;</label>
+                            <input type="text" name="shop" id="shop"></input>
+                        </div>
+                        <div>
+                            <label>Filter by price (Total):&nbsp;&nbsp;&nbsp;</label>
+                            <label for="priceLow">From&nbsp;</label>
+                            <input type="number" name="priceLow" id="priceLow"></input>
+                            <label for="priceHigh">&nbsp;To&nbsp;</label>
+                            <input type="number" name="priceHigh" id="priceHigh"></input>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <table class="orderTable" {...getTableProps()}>
                 <thead>
                     {headerGroups.map(headerGroup => (
                         <tr {...headerGroup.getHeaderGroupProps()}>
                             {headerGroup.headers.map(column => (
-                                // Add the sorting props to control sorting. For this example
-                                // we can add them into the header props
                                 <th {...column.getHeaderProps(column.getSortByToggleProps())}>
                                     {column.render('Header')}
-                                    {/* Add a sort direction indicator */}
                                     <span>
                                         {column.isSorted
                                             ? column.isSortedDesc
@@ -45,7 +116,7 @@ function Table({ columns, data }) {
                     ))}
                 </thead>
                 <tbody {...getTableBodyProps()}>
-                    {rows.map(
+                    {page.map(
                         (row, i) => {
                             prepareRow(row);
                             return (
@@ -61,6 +132,14 @@ function Table({ columns, data }) {
                     )}
                 </tbody>
             </table>
+            <div className="pagination">
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    {'Previous'}
+                </button>{' '}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                    {'Next'}
+                </button>{' '}
+            </div>
         </>
     )
 }
@@ -130,67 +209,13 @@ function ShippedProducts() {
         []
     )
 
-    function handleFilterButtonClick() {
-        var filterDiv = document.getElementById("hiddenFilterDiv")
-        if (filterDiv.style.display == 'flex')
-            filterDiv.style.display = 'none'
-        else
-            filterDiv.style.display = 'flex'
-    }
+    
 
     return (
         <>
             <h1>Orders</h1>
             <div class="mainDiv">
-                <div class="mainFilterDiv">
-                    <div class="filterDiv">
-                        <div>
-                            Show&nbsp;
-                            <select>
-                                {[10, 20, 30, 40].map(option => <option>{option}</option>)}
-                            </select>
-                            &nbsp;entires
-                        </div>
-                        <div>
-                            <button onClick={handleFilterButtonClick}>
-                                <img src={require("./filter.png")} alt="filter icon" width="25" height="25"></img>
-                            </button>
-                        </div>
-                    </div>
-                    <div id="hiddenFilterDiv">
-                        <div id="insideHiddenFilterDiv">
-                            <div>
-                                <label for="date">Filter by date&nbsp;</label>
-                                <input type="date" name="date" id="date"></input>
-                            </div>
-                            <div>
-                                <label for="name">Filter by name&nbsp;</label>
-                                <input type="text" name="name" id="name"></input>
-                            </div>
-                            <div>
-                                <label for="category">Category&nbsp;</label>
-                                <select id="category">
-                                    <option>Fruits and Vegetables</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div id="insideHiddenFilterDiv2">
-                            <div>
-                                <label for="shop">Filter by shop&nbsp;</label>
-                                <input type="text" name="shop" id="shop"></input>
-                            </div>
-                            <div>
-                                <label>Filter by price (Total):&nbsp;&nbsp;&nbsp;</label>
-                                <label for="priceLow">From&nbsp;</label>
-                                <input type="number" name="priceLow" id="priceLow"></input>
-                                <label for="priceHigh">&nbsp;To&nbsp;</label>
-                                <input type="number" name="priceHigh" id="priceHigh"></input>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <Table columns={columns} data={orders} />
-                <div class="paging">Ovdje ce ici paging 1, 2, 3...</div>
             </div>
         </>
     )
