3afc1dfd49da16da8d957c4e91b27f995e4f223a
Dzenan Nuhic <dnuhic1@etf.unsa.ba>
2022-04-24 01:25:46 +0200

    added UserOrderModel and ProductUserOrderIntertable

---
+++ SET3-Backend/Models/ProductUserOrderIntertable.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SET3_Backend.Models
+{
+    public class ProductUserOrderIntertable
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserOrderId { get; set; }
+        public int ProductId { get; set; }
+        public double Quantity { get; set; }
+
+        public ProductUserOrderIntertable()
+        {
+        }
+
+        public ProductUserOrderIntertable(int id, int userOrderId, int productId, double quantity)
+        {
+            Id = id;
+            UserOrderId = userOrderId;
+            ProductId = productId;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ SET3-Backend/Models/UserOrderModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SET3_Backend.Models
+{
+    public class UserOrderModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool Done { get; set; }
+        public int ShopId { get; set; }
+        public int UserId { get; set; }
+
+        public UserOrderModel()
+        {
+        }
+
+        public UserOrderModel(int id, DateTime createdDate, bool done, int shopId, int userId)
+        {
+            Id = id;
+            CreatedDate = createdDate;
+            Done = done;
+            ShopId = shopId;
+            UserId = userId;
+        }
+    }
+}
