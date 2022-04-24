31d1323d1374db73b1e98d385282ab3b0dd7bf31
Dzenan Nuhic <dnuhic1@etf.unsa.ba>
2022-04-24 21:11:19 +0200

    added backend routes for creating an user order and adding products to it.

--- SET3-Backend/Controllers/UserOrderModelsController.cs
+++ SET3-Backend/Controllers/UserOrderModelsController.cs
@@ -16,10 +16,142 @@ namespace SET3_Backend.Controllers
     public class UserOrderModelsController : ControllerBase
     {
         private readonly Context _context;
+        private readonly IConfiguration _configuration;
 
-        public UserOrderModelsController(Context context)
+        public UserOrderModelsController(Context context, IConfiguration configuration)
         {
+            _configuration = configuration;
             _context = context;
+        }
+
+
+        #region DataTransferObjects
+        public class ProductWithShopQuantityDto {
+            public int ProductId { get; set; }
+            public string Name { get; set; }
+            public string CategoryName { get; set; }
+            public double Quantity { get; set; }
+            public string Barcode { get; set; }
+            public string BarcodeText { get; set; }
+            public float Price { get; set; }
+
+            public ProductWithShopQuantityDto(int productId, string name, string categoryName,
+                double quantity, string barcode, string barcodeText, float price)
+            {
+                ProductId = productId;
+                Name = name;
+                CategoryName = categoryName;
+                Quantity = quantity;
+                Barcode = barcode;
+                BarcodeText = barcodeText;
+                Price = price;
+            }
+        }
+
+        public class ProductQuantityDto
+        {
+            public int ProductId { get; set; }
+            public double Quantity { get; set; }
+
+            public ProductQuantityDto()
+            {
+            }
+
+            public ProductQuantityDto(int productId, double quantity)
+            {
+                ProductId = productId;
+                Quantity = quantity;
+            }
+        }
+
+        public class UserOrderDto
+        {
+            public int ShopId { get; set; }
+            public int UserId { get; set; }
+            public List<ProductQuantityDto> ProductQuantitys { get; set; }
+
+            public UserOrderDto()
+            {
+            }
+
+            public UserOrderDto(int shopId, int userId, List<ProductQuantityDto> productQuantitys)
+            {
+                ShopId = shopId;
+                UserId = userId;
+                this.ProductQuantitys = productQuantitys;
+            }
+        }
+
+        #endregion
+
+
+        [HttpGet("productsFromShop/{id}")]
+        public async Task<ActionResult<IEnumerable<ProductWithShopQuantityDto>>> GetProductsFromShop(int id)
+        {
+            //treba dodati neke provjere ako je lose kreirano u bazi, da nema nesto u tabeli
+            List<ProductWithShopQuantityDto> productsDto = new List<ProductWithShopQuantityDto>();
+
+            List<ProductShopIntertable> productShops = await _context.ProductShopIntertables.Where(productShop => productShop.ShopId == id).ToListAsync();
+            IEnumerable<Tuple<int, double>> productIds = productShops.Select(productShop => new Tuple<int, double>(productShop.ProductId, productShop.Quantity));
+
+            List<ProductModel> products = await _context.ProductModels.ToListAsync();
+
+            products = products.Where(product => productIds.Where(productId => productId.Item1 == product.Id).Count() > 0).ToList();
+            try
+            {
+                products.ForEach(product =>
+                {
+                    Tuple<int, double> quantityTuple = productIds.Where(productId => productId.Item1 == product.Id).FirstOrDefault();
+                    if (quantityTuple == null)
+                        throw new ArgumentException("Greska (Placeholder)");
+                    productsDto.Add(new ProductWithShopQuantityDto(product.Id, product.Name, product.CategoryName, quantityTuple.Item2, product.Barcode, product.BarcodeText, product.Price));
+                });
+            } catch (Exception ex)
+            {
+                return BadRequest("Greska");
+            }
+
+            return productsDto;
+        }
+
+        [HttpPost("save")]
+        public async Task<ActionResult<UserOrderModel>> SaveUserOrder([FromBody] UserOrderDto userOrderDto)
+        {
+            //treba dodati provjere za quantity
+            UserOrderModel userOrder = new UserOrderModel(DateTime.Now, false, userOrderDto.ShopId, userOrderDto.UserId);
+            _context.UserOrderModels.Add(userOrder);
+            await _context.SaveChangesAsync();
+
+            List<ProductUserOrderIntertable> products = new List<ProductUserOrderIntertable>();
+            userOrderDto.ProductQuantitys.ForEach(productQuantity =>
+            {
+                products.Add(new ProductUserOrderIntertable(userOrder.Id, productQuantity.ProductId, productQuantity.Quantity));
+            });
+
+            await _context.ProductUserOrderIntertables.AddRangeAsync(products);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserOrderModel", new { id = userOrder.Id }, userOrder);
+        }
+
+        [HttpPost("finish")]
+        public async Task<IActionResult> FinishUserOrder([FromBody] UserOrderDto userOrderDto)
+        {
+            //treba dodati provjere za quantity
+            UserOrderModel userOrder = new UserOrderModel(DateTime.Now, true, userOrderDto.ShopId, userOrderDto.UserId);
+            _context.UserOrderModels.Add(userOrder);
+            await _context.SaveChangesAsync();
+
+            List<ProductUserOrderIntertable> products = new List<ProductUserOrderIntertable>();
+            userOrderDto.ProductQuantitys.ForEach(productQuantity =>
+            {
+                products.Add(new ProductUserOrderIntertable(userOrder.Id, productQuantity.ProductId, productQuantity.Quantity));
+            });
+
+            await _context.ProductUserOrderIntertables.AddRangeAsync(products);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserOrderModel", new { id = userOrder.Id }, userOrder);
         }
 
         // GET: api/UserOrderModels
@@ -74,16 +206,16 @@ namespace SET3_Backend.Controllers
             return NoContent();
         }
 
-        // POST: api/UserOrderModels
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<UserOrderModel>> PostUserOrderModel(UserOrderModel userOrderModel)
-        {
-            _context.UserOrderModels.Add(userOrderModel);
-            await _context.SaveChangesAsync();
+        //// POST: api/UserOrderModels
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPost]
+        //public async Task<ActionResult<UserOrderModel>> PostUserOrderModel(UserOrderModel userOrderModel)
+        //{
+        //    _context.UserOrderModels.Add(userOrderModel);
+        //    await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetUserOrderModel", new { id = userOrderModel.Id }, userOrderModel);
-        }
+        //    return CreatedAtAction("GetUserOrderModel", new { id = userOrderModel.Id }, userOrderModel);
+        //}
 
         // DELETE: api/UserOrderModels/5
         [HttpDelete("{id}")]
