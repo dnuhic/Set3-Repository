426b41d24e1549fc93225a49d29c3e47e0352ad5
Dzenan Nuhic <dnuhic1@etf.unsa.ba>
2022-04-28 05:17:42 +0200

    added routes needed for editing a user order

--- SET3-Backend/Controllers/UserOrderModelsController.cs
+++ SET3-Backend/Controllers/UserOrderModelsController.cs
@@ -86,6 +86,33 @@ namespace SET3_Backend.Controllers
             }
         }
 
+        public class ProductWithShopAndChosenQuantityDto
+        {
+            public int ProductId { get; set; }
+            public string Name { get; set; }
+            public string CategoryName { get; set; }
+            public double Quantity { get; set; }
+            public double QuantityChosen { get; set; }
+            public string Barcode { get; set; }
+            public string BarcodeText { get; set; }
+            public float Price { get; set; }
+            public string MeasuringUnit { get; set; }
+
+            public ProductWithShopAndChosenQuantityDto(int productId, string name, string categoryName, 
+                double quantity, double quantityChosen, string barcode, string barcodeText, float price, string measuringUnit)
+            {
+                ProductId = productId;
+                Name = name;
+                CategoryName = categoryName;
+                Quantity = quantity;
+                QuantityChosen = quantityChosen;
+                Barcode = barcode;
+                BarcodeText = barcodeText;
+                Price = price;
+                MeasuringUnit = measuringUnit;
+            }
+        }
+
         #endregion
 
 
@@ -114,7 +141,6 @@ namespace SET3_Backend.Controllers
             {
                 return BadRequest("Greska");
             }
-
             return productsDto;
         }
 
@@ -158,11 +184,55 @@ namespace SET3_Backend.Controllers
             return CreatedAtAction("GetUserOrderModel", new { id = userOrder.Id }, userOrder);
         }
 
+        [HttpGet("savedUserOrderProducts/{id}")]
+        public async Task<ActionResult<IEnumerable<ProductWithShopAndChosenQuantityDto>>> GetSavedUserOrderProducts(int id)
+        {
+            UserOrderModel userOrderModel = await _context.UserOrderModels.FindAsync(id);
+
+            List<ProductWithShopAndChosenQuantityDto> productsDto = new List<ProductWithShopAndChosenQuantityDto>();
+
+            List<ProductShopIntertable> productShops = await _context.ProductShopIntertables.Where(productShop => productShop.ShopId == userOrderModel.ShopId).ToListAsync();
+            IEnumerable<Tuple<int, double>> productIds = productShops.Select(productShop => new Tuple<int, double>(productShop.ProductId, productShop.Quantity));
+
+
+            List<ProductUserOrderIntertable> userOrderProducts = await _context.ProductUserOrderIntertables.Where(userOrderProduct => userOrderProduct.UserOrderId == id).ToListAsync();
+            IEnumerable<Tuple<int, double>> chosenQuantityProducts = userOrderProducts.Select(userOrderProduct => new Tuple<int, double>(userOrderProduct.ProductId, userOrderProduct.Quantity));
+
+            List<ProductModel> products = await _context.ProductModels.ToListAsync();
+            
+            products = products.Where(product => productIds.Where(productId => productId.Item1 == product.Id).Count() > 0).ToList();
+            try
+            {
+                products.ForEach(product =>
+                {
+                    Tuple<int, double> quantityTuple = productIds.Where(productId => productId.Item1 == product.Id).FirstOrDefault();
+                    Tuple<int, double> chosenQuantityTuple = chosenQuantityProducts.Where(chosenQuantityProduct => chosenQuantityProduct.Item1 == product.Id).FirstOrDefault();
+                    if (quantityTuple == null)
+                        throw new ArgumentException("Greska (Placeholder)");
+                    if (chosenQuantityTuple == null)
+                        productsDto.Add(new ProductWithShopAndChosenQuantityDto(product.Id, product.Name, product.CategoryName, quantityTuple.Item2, 0, product.Barcode, product.BarcodeText, product.Price, product.MeasuringUnit));
+                    else 
+                        productsDto.Add(new ProductWithShopAndChosenQuantityDto(product.Id, product.Name, product.CategoryName, quantityTuple.Item2, chosenQuantityTuple.Item2, product.Barcode, product.BarcodeText, product.Price, product.MeasuringUnit));
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Greska");
+            }
+            return productsDto;
+        }
+
         // GET: api/UserOrderModels
         [HttpGet]
         public async Task<ActionResult<IEnumerable<UserOrderModel>>> GetUserOrderModels()
         {
             return await _context.UserOrderModels.ToListAsync();
+        }
+
+        [HttpGet("myuserorders/{id}")]
+        public async Task<ActionResult<IEnumerable<UserOrderModel>>> GetUserOrderModels(int id)
+        {
+            return await _context.UserOrderModels.Where(userOrderModel => userOrderModel.UserId == id).ToListAsync();
         }
 
         // GET: api/UserOrderModels/5
@@ -178,6 +248,95 @@ namespace SET3_Backend.Controllers
 
             return userOrderModel;
         }
+
+        [HttpPut("save/{id}")]
+        public async Task<IActionResult> EditUserOrderModelSave(int id, [FromBody] UserOrderDto userOrderDto)
+        {
+            UserOrderModel userOrderModel = await _context.UserOrderModels.FindAsync(id);
+
+            userOrderModel.UserId = userOrderDto.UserId;
+            userOrderModel.ShopId = userOrderDto.ShopId;
+            userOrderModel.UpdatedDate = DateTime.Now;
+            userOrderModel.CashRegisterId = userOrderDto.CashRegisterId;
+            userOrderModel.Done = false;
+            _context.UserOrderModels.Update(userOrderModel);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                List<ProductUserOrderIntertable> productUserOrderIntertables = await _context.ProductUserOrderIntertables
+                    .Where(productUserOrderIntertable => productUserOrderIntertable.UserOrderId == id).ToListAsync();
+                _context.ProductUserOrderIntertables.RemoveRange(productUserOrderIntertables);
+                await _context.SaveChangesAsync();
+
+                List<ProductUserOrderIntertable> products = new List<ProductUserOrderIntertable>();
+                userOrderDto.ProductQuantitys.ForEach(productQuantity =>
+                {
+                    products.Add(new ProductUserOrderIntertable(id, productQuantity.ProductId, productQuantity.Quantity));
+                });
+
+                await _context.ProductUserOrderIntertables.AddRangeAsync(products);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserOrderModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPut("finish/{id}")]
+        public async Task<IActionResult> EditUserOrderModelFinish(int id, [FromBody] UserOrderDto userOrderDto)
+        {
+            UserOrderModel userOrderModel = await _context.UserOrderModels.FindAsync(id);
+
+            userOrderModel.UserId = userOrderDto.UserId;
+            userOrderModel.ShopId = userOrderDto.ShopId;
+            userOrderModel.UpdatedDate = DateTime.Now;
+            userOrderModel.CashRegisterId = userOrderDto.CashRegisterId;
+            userOrderModel.Done = true;
+            _context.UserOrderModels.Update(userOrderModel);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                List<ProductUserOrderIntertable> productUserOrderIntertables = await _context.ProductUserOrderIntertables
+                    .Where(productUserOrderIntertable => productUserOrderIntertable.UserOrderId == id).ToListAsync();
+                _context.ProductUserOrderIntertables.RemoveRange(productUserOrderIntertables);
+                await _context.SaveChangesAsync();
+
+                List<ProductUserOrderIntertable> products = new List<ProductUserOrderIntertable>();
+                userOrderDto.ProductQuantitys.ForEach(productQuantity =>
+                {
+                    products.Add(new ProductUserOrderIntertable(id, productQuantity.ProductId, productQuantity.Quantity));
+                });
+
+                await _context.ProductUserOrderIntertables.AddRangeAsync(products);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserOrderModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
 
         // PUT: api/UserOrderModels/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
@@ -230,7 +389,9 @@ namespace SET3_Backend.Controllers
             {
                 return NotFound();
             }
-
+            List<ProductUserOrderIntertable> productUserOrderIntertables = await _context.ProductUserOrderIntertables.Where(x => x.UserOrderId == id).ToListAsync();
+            _context.ProductUserOrderIntertables.RemoveRange(productUserOrderIntertables);
+            await _context.SaveChangesAsync();
             _context.UserOrderModels.Remove(userOrderModel);
             await _context.SaveChangesAsync();
 
