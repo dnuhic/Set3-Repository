c110addff6e2d9d10638b5a05f1645bfed528cf2
eimamovic2 <eimamovic2@etf.unsa.ba>
2022-05-04 12:02:54 +0200

    Izmjene modela i baze

--- SET3-Backend/Database/Context.cs
+++ SET3-Backend/Database/Context.cs
@@ -24,7 +24,7 @@ namespace SET3_Backend.Database
         public virtual DbSet<ProductUserOrderIntertable> ProductUserOrderIntertables { get; set; }
         public virtual DbSet<MeasuringUnit> MeasuringUnits { get; set; }
         public virtual DbSet<TableModel> TableModels { get; set; }
-
+        public virtual DbSet<FiscalBillModel> FiscalBillModels { get; set; }
         public Context([NotNull] DbContextOptions<Context> options) : base(options) {
             var conn = (Microsoft.Data.SqlClient.SqlConnection)Database.GetDbConnection();
         }
