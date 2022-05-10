979e9548fc0c9563083f325c76294d08bf71877a
Dzenan Nuhic <dnuhic1@etf.unsa.ba>
2022-05-10 22:13:25 +0200

    Added .env file creation

--- setup.bat
+++ setup.bat
@@ -6,6 +6,8 @@ cd Set3-Database
 start DatabaseScript.exe
 cd ..
 cd Set3-Frontend
+type nul > .env.development.local
+echo REACT_APP_BACKEND_URL=https://localhost:7194/ > .env.development.local
 call npm install
 call npm update
 call npm install 
