4b46f830475013e7f6e3953016eb40b0415a5633
eimamovic2 <eimamovic2@etf.unsa.ba>
2022-05-30 23:55:31 +0200

    Generisanje pdfa part  1

---
+++ SET3-Backend/Controllers/GeneratingPDFController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Drawing;
+using System.IO;
+using SET3_Backend.Database;
+using Syncfusion.Pdf.Grid;
+using Microsoft.AspNetCore.Hosting;
+using System.Data;
+
+namespace SET3_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeneratingPDFController : Controller
+    {
+        private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment _hostingEnvironment;
+        private readonly Context _context;
+        private readonly IConfiguration _configuration;
+
+        public GeneratingPDFController(Context context, IConfiguration configuration, Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment)
+        {
+           
+            _hostingEnvironment = hostingEnvironment;
+            _configuration = configuration;
+            _context = context;
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public async Task<ActionResult> CreateDocument()
+        {
+            //Creates a new PDF document
+            PdfDocument document = new PdfDocument();
+            //Adds page settings
+            document.PageSettings.Orientation = PdfPageOrientation.Landscape;
+            document.PageSettings.Margins.All = 50;
+            //Adds a page to the document
+            PdfPage page = document.Pages.Add();
+            PdfGraphics graphics = page.Graphics;
+            //Loads the image as stream
+            FileStream imageStream = new FileStream("AdventureCycle.jpg", FileMode.Open, FileAccess.Read);
+            RectangleF bounds = new RectangleF(176, 0, 390, 130);
+            PdfImage image = PdfImage.FromStream(imageStream);
+            //Draws the image to the PDF page
+            page.Graphics.DrawImage(image, bounds);
+
+            PdfBrush solidBrush = new PdfSolidBrush(new PdfColor(126, 151, 173));
+            bounds = new RectangleF(0, bounds.Bottom + 90, graphics.ClientSize.Width, 30);
+            //Draws a rectangle to place the heading in that region.
+            graphics.DrawRectangle(solidBrush, bounds);
+            //Creates a font for adding the heading in the page
+            PdfFont subHeadingFont = new PdfStandardFont(PdfFontFamily.TimesRoman, 14);
+            //Creates a text element to add the invoice number
+            PdfTextElement element = new PdfTextElement("INVOICE " + id.ToString(), subHeadingFont);
+            element.Brush = PdfBrushes.White;
+
+            //Draws the heading on the page
+            PdfLayoutResult result = element.Draw(page, new PointF(10, bounds.Top + 8));
+            string currentDate = "DATE " + DateTime.Now.ToString("MM/dd/yyyy");
+            //Measures the width of the text to place it in the correct location
+            SizeF textSize = subHeadingFont.MeasureString(currentDate);
+            PointF textPosition = new PointF(graphics.ClientSize.Width - textSize.Width - 10, result.Bounds.Y);
+            //Draws the date by using DrawString method
+            graphics.DrawString(currentDate, subHeadingFont, element.Brush, textPosition);
+            PdfFont timesRoman = new PdfStandardFont(PdfFontFamily.TimesRoman, 10);
+            //Creates text elements to add the address and draw it to the page.
+            element = new PdfTextElement("BILL TO ", timesRoman);
+            element.Brush = new PdfSolidBrush(new PdfColor(126, 155, 203));
+            result = element.Draw(page, new PointF(10, result.Bounds.Bottom + 25));
+            PdfPen linePen = new PdfPen(new PdfColor(126, 151, 173), 0.70f);
+            PointF startPoint = new PointF(0, result.Bounds.Bottom + 3);
+            PointF endPoint = new PointF(graphics.ClientSize.Width, result.Bounds.Bottom + 3);
+            //Draws a line at the bottom of the address
+            graphics.DrawLine(linePen, startPoint, endPoint);
+
+            //Creates the datasource for the table
+            DataTable invoiceDetails = GetProductDetailsAsDataTable(); //Pronaci ovu metodu
+            //Creates a PDF grid
+            PdfGrid grid = new PdfGrid();
+            //Adds the data source
+            grid.DataSource = invoiceDetails;
+            //Creates the grid cell styles
+            PdfGridCellStyle cellStyle = new PdfGridCellStyle();
+            cellStyle.Borders.All = PdfPens.White;
+            PdfGridRow header = grid.Headers[0];
+            //Creates the header style
+            PdfGridCellStyle headerStyle = new PdfGridCellStyle();
+            headerStyle.Borders.All = new PdfPen(new PdfColor(126, 151, 173));
+            headerStyle.BackgroundBrush = new PdfSolidBrush(new PdfColor(126, 151, 173));
+            headerStyle.TextBrush = PdfBrushes.White;
+            headerStyle.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 14f, PdfFontStyle.Regular);
+
+            //Adds cell customizations
+            for (int i = 0; i < header.Cells.Count; i++)
+            {
+                if (i == 0 || i == 1)
+                    header.Cells[i].StringFormat = new PdfStringFormat(PdfTextAlignment.Left, PdfVerticalAlignment.Middle);
+                else
+                    header.Cells[i].StringFormat = new PdfStringFormat(PdfTextAlignment.Right, PdfVerticalAlignment.Middle);
+            }
+
+            //Applies the header style
+            header.ApplyStyle(headerStyle);
+            cellStyle.Borders.Bottom = new PdfPen(new PdfColor(217, 217, 217), 0.70f);
+            cellStyle.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 12f);
+            cellStyle.TextBrush = new PdfSolidBrush(new PdfColor(131, 130, 136));
+            //Creates the layout format for grid
+            PdfGridLayoutFormat layoutFormat = new PdfGridLayoutFormat();
+            // Creates layout format settings to allow the table pagination
+            layoutFormat.Layout = PdfLayoutType.Paginate;
+            //Draws the grid to the PDF page.
+            PdfGridLayoutResult gridResult = grid.Draw(page, new RectangleF(new PointF(0, result.Bounds.Bottom + 40), new SizeF(graphics.ClientSize.Width, graphics.ClientSize.Height - 100)), layoutFormat);
+
+            FileStream fileStream = new FileStream("Sample.pdf", FileMode.CreateNew, FileAccess.ReadWrite);
+            //Save and close the PDF document 
+            document.Save(fileStream);
+            document.Close(true);
+        }
+    }
+}
