e2a0671d9bddde50625d5f4c7fe34059e1456a4f
eimamovic2 <eimamovic2@etf.unsa.ba>
2022-05-31 23:02:14 +0200

    PDF part 2

--- SET3-Backend/Controllers/GeneratingPDFController.cs
+++ SET3-Backend/Controllers/GeneratingPDFController.cs
@@ -29,92 +29,71 @@ namespace SET3_Backend.Controllers
         [HttpGet]
         public async Task<ActionResult> CreateDocument()
         {
-            //Creates a new PDF document
-            PdfDocument document = new PdfDocument();
-            //Adds page settings
-            document.PageSettings.Orientation = PdfPageOrientation.Landscape;
-            document.PageSettings.Margins.All = 50;
-            //Adds a page to the document
-            PdfPage page = document.Pages.Add();
-            PdfGraphics graphics = page.Graphics;
-            //Loads the image as stream
-            FileStream imageStream = new FileStream("AdventureCycle.jpg", FileMode.Open, FileAccess.Read);
-            RectangleF bounds = new RectangleF(176, 0, 390, 130);
-            PdfImage image = PdfImage.FromStream(imageStream);
-            //Draws the image to the PDF page
-            page.Graphics.DrawImage(image, bounds);
+            //Create a new PDF document
+            PdfDocument doc = new PdfDocument();
+            //Naslovna
 
-            PdfBrush solidBrush = new PdfSolidBrush(new PdfColor(126, 151, 173));
-            bounds = new RectangleF(0, bounds.Bottom + 90, graphics.ClientSize.Width, 30);
-            //Draws a rectangle to place the heading in that region.
-            graphics.DrawRectangle(solidBrush, bounds);
-            //Creates a font for adding the heading in the page
-            PdfFont subHeadingFont = new PdfStandardFont(PdfFontFamily.TimesRoman, 14);
-            //Creates a text element to add the invoice number
-            PdfTextElement element = new PdfTextElement("INVOICE " + id.ToString(), subHeadingFont);
-            element.Brush = PdfBrushes.White;
 
-            //Draws the heading on the page
-            PdfLayoutResult result = element.Draw(page, new PointF(10, bounds.Top + 8));
-            string currentDate = "DATE " + DateTime.Now.ToString("MM/dd/yyyy");
-            //Measures the width of the text to place it in the correct location
-            SizeF textSize = subHeadingFont.MeasureString(currentDate);
-            PointF textPosition = new PointF(graphics.ClientSize.Width - textSize.Width - 10, result.Bounds.Y);
-            //Draws the date by using DrawString method
-            graphics.DrawString(currentDate, subHeadingFont, element.Brush, textPosition);
-            PdfFont timesRoman = new PdfStandardFont(PdfFontFamily.TimesRoman, 10);
-            //Creates text elements to add the address and draw it to the page.
-            element = new PdfTextElement("BILL TO ", timesRoman);
-            element.Brush = new PdfSolidBrush(new PdfColor(126, 155, 203));
-            result = element.Draw(page, new PointF(10, result.Bounds.Bottom + 25));
-            PdfPen linePen = new PdfPen(new PdfColor(126, 151, 173), 0.70f);
-            PointF startPoint = new PointF(0, result.Bounds.Bottom + 3);
-            PointF endPoint = new PointF(graphics.ClientSize.Width, result.Bounds.Bottom + 3);
-            //Draws a line at the bottom of the address
-            graphics.DrawLine(linePen, startPoint, endPoint);
 
-            //Creates the datasource for the table
-            DataTable invoiceDetails = GetProductDetailsAsDataTable(); //Pronaci ovu metodu
-            //Creates a PDF grid
-            PdfGrid grid = new PdfGrid();
-            //Adds the data source
-            grid.DataSource = invoiceDetails;
-            //Creates the grid cell styles
-            PdfGridCellStyle cellStyle = new PdfGridCellStyle();
-            cellStyle.Borders.All = PdfPens.White;
-            PdfGridRow header = grid.Headers[0];
-            //Creates the header style
-            PdfGridCellStyle headerStyle = new PdfGridCellStyle();
-            headerStyle.Borders.All = new PdfPen(new PdfColor(126, 151, 173));
-            headerStyle.BackgroundBrush = new PdfSolidBrush(new PdfColor(126, 151, 173));
-            headerStyle.TextBrush = PdfBrushes.White;
-            headerStyle.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 14f, PdfFontStyle.Regular);
 
-            //Adds cell customizations
-            for (int i = 0; i < header.Cells.Count; i++)
-            {
-                if (i == 0 || i == 1)
-                    header.Cells[i].StringFormat = new PdfStringFormat(PdfTextAlignment.Left, PdfVerticalAlignment.Middle);
-                else
-                    header.Cells[i].StringFormat = new PdfStringFormat(PdfTextAlignment.Right, PdfVerticalAlignment.Middle);
-            }
 
-            //Applies the header style
-            header.ApplyStyle(headerStyle);
-            cellStyle.Borders.Bottom = new PdfPen(new PdfColor(217, 217, 217), 0.70f);
-            cellStyle.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 12f);
-            cellStyle.TextBrush = new PdfSolidBrush(new PdfColor(131, 130, 136));
-            //Creates the layout format for grid
-            PdfGridLayoutFormat layoutFormat = new PdfGridLayoutFormat();
-            // Creates layout format settings to allow the table pagination
-            layoutFormat.Layout = PdfLayoutType.Paginate;
-            //Draws the grid to the PDF page.
-            PdfGridLayoutResult gridResult = grid.Draw(page, new RectangleF(new PointF(0, result.Bounds.Bottom + 40), new SizeF(graphics.ClientSize.Width, graphics.ClientSize.Height - 100)), layoutFormat);
 
-            FileStream fileStream = new FileStream("Sample.pdf", FileMode.CreateNew, FileAccess.ReadWrite);
-            //Save and close the PDF document 
-            document.Save(fileStream);
-            document.Close(true);
+            //Kreiranje tabele
+
+            //Add a page
+            PdfPage page = doc.Pages.Add();
+            //Create a PdfGrid
+            PdfGrid pdfGrid = new PdfGrid();
+            //Create a DataTable
+            DataTable dataTable = new DataTable();
+            //Add columns to the DataTable
+            /*
+            dataTable.Columns.Add("Name");
+            dataTable.Columns.Add("Category Name");
+            dataTable.Columns.Add("Quantity");
+            dataTable.Columns.Add("Measuring Unit");
+            dataTable.Columns.Add("Price");
+            */
+            dataTable.Columns.Add("ProductID");
+            dataTable.Columns.Add("ProductName");
+            dataTable.Columns.Add("Quantity");
+            dataTable.Columns.Add("UnitPrice");
+            dataTable.Columns.Add("Discount");
+            dataTable.Columns.Add("Price");
+            //Add rows to the DataTable
+            dataTable.Rows.Add(new object[] { "CA-1098", "Queso Cabrales", "12", "14", "1", "167" });
+            dataTable.Rows.Add(new object[] { "LJ-0192-M", "Singaporean Hokkien Fried Mee", "10", "20", "3", "197" });
+            dataTable.Rows.Add(new object[] { "SO-B909-M", "Mozzarella di Giovanni", "15", "65", "10", "956" });
+            //Assign data source
+            pdfGrid.DataSource = dataTable;
+            //Draw grid to the page of PDF document
+            pdfGrid.Draw(page, new PointF(10, 10));
+
+            //Slika charta za tabelu iznad
+
+            PdfPage page2 = doc.Pages.Add();
+            //Create PDF graphics for the page
+            PdfGraphics graphics = page2.Graphics;
+            //Load the image as stream.
+            FileStream imageStream = new FileStream("Materials/Logo.png", FileMode.Open, FileAccess.Read);
+            PdfBitmap image = new PdfBitmap(imageStream);
+            //Draw the image
+            graphics.DrawImage(image, new RectangleF(0, 200, 500, 500));
+            //Save the PDF document to stream
+
+            //Spasavanje dokumenta
+            MemoryStream stream = new MemoryStream();
+            doc.Save(stream);
+            //If the position is not set to '0' then the PDF will be empty.
+            stream.Position = 0;
+            //Close the document.
+            doc.Close(true);
+            //Defining the ContentType for pdf file.
+            string contentType = "application/pdf";
+            //Define the file name.
+            string fileName = "Proba.pdf";
+            //Creates a FileContentResult object by using the file contents, content type, and file name.
+            return File(stream, contentType, fileName);
         }
     }
 }
