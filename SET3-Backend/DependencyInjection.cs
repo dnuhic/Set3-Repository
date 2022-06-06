81b6459205afea3f8ea965bd6a84df4c4bf48057
abegovac2 <begovac5@gmail.com>
2022-06-06 17:50:10 +0200

    windows fix

--- SET3-Backend/DependencyInjection.cs
+++ SET3-Backend/DependencyInjection.cs
@@ -15,7 +15,7 @@ namespace SET3_Backend
                 //Treba dodati connection string na bazu umjesto 'dbConnString'
                 //var dbConnString = @"Server=set3.database.windows.net;Initial Catalog=Set3Baza;Persist Security Info=False;User ID=set3admin;Password=prir0da#aj;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
                 //var dbConnString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LokalnaBaza;User=set3admin;Password=prir0da#aj;";
-                var dbConnString = @"Data Source=localhost,1433;Initial Catalog=Set3Baza;User ID=SA;Password=prir0da#aj;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
+                var dbConnString = @"Data Source=localhost,1433;Initial Catalog=Set3Baza;User ID=SA;Password=prir0da#ajprir0da#aj;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
                 options.UseSqlServer(dbConnString,
                     dboContextOptions => dboContextOptions.EnableRetryOnFailure(2));
             });
